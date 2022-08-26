905608056837c4716358e23cf9063f7e79460c98
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-26 14:54:53 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const btn=document.querySelector('#btn')
+
+let name = "Oussama"
+let email = "oussama@masri.com"
+btn.addEventListener('click',submitData(name,email))
+
+function submitData(fname, emailAddress) {
+const person={
+    name:fname,
+    email:emailAddress
+}
+    const configureObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( person )
+    }
+    
+    return fetch('http://localhost:3000/users', configureObject )
+      .then( response => {
+        return response.json()
+      } )
+      .then( data => {
+        document.body.innerHTML = data[ "id" ]
+      } )
+      .catch( err => {
+        document.body.innerHTML = err.message
+      } )
+  }
